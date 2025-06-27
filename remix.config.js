531b2b70c88b4a3a1ab51c3ae459b8535612d557
ta543 <tobias@tobiasa.com>
2025-06-27 19:29:16 +0200

    dwdwdwdwdw

--- remix.config.js
+++ remix.config.js
@@ -13,6 +13,7 @@ if (
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverBuildDirectory: "dist",
   appDirectory: "app",
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
